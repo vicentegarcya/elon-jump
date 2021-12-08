81c9a265a2075dc1049f4ee75828b367fbd1642b
Vicente Garcia <vicentegarciaayllon@gmail.com>
2021-12-08 14:31:45 +0100

    add and style logo, email image, background

--- assets/js/models/background.js
+++ assets/js/models/background.js
@@ -7,7 +7,7 @@ class Background {
         this.height = this.ctx.canvas.height;
 
         this.img = new Image();
-        this.img.src = "assets/images/background.png";
+        this.img.src = "../assets/images/background.png";
         this.img.isReady = false;
         this.img.onload = () => {
             this.img.isReady = true;
--- assets/js/models/game.js
+++ assets/js/models/game.js
@@ -1,19 +1,33 @@
 class Game {
     constructor(ctx){
         this.ctx = ctx;
-
         this.background = new Background(ctx);
+        this.intervalId = undefined;
+        this.fps = 1000/60;
 
     }
 
     start(){
+        if(!this.intervalId){
+            this.intervalId = setInterval(() => {
+                
+                this.clear();
 
-        //this.clear();
+                this.move();
 
-        //this.move();
+                this.draw();
 
-        this.draw();
+            }, this.fps)
+        }
 
+    }
+
+    clear(){
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    }
+
+    move(){
+        //...
     }
 
     draw(){
