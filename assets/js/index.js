c270f427a3b55746ad31796da82ecb02feb59ced
Vicente Garcia <vicentegarciaayllon@gmail.com>
2021-12-15 21:06:34 +0100

    add predefined player

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('my-canvas');
 const ctx = canvas.getContext('2d');
 
 const game = new Game(ctx);
+game.setPlayer('red-sprite');
 
 window.onload = () => {
     document.getElementById('start-btn').onclick = () => {
